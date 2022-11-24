6d780b26db969d9303c500e7c9edc5e841102d2b
Paulo Teixeira <paulo.teixeira@fortbrasil.com.br>
2022-11-24 12:45:09 -0300

    Finish documentation.

---
+++ src/shared/container/index.ts
@@ -0,0 +1,16 @@
+import { container } from "tsyringe";
+
+import { CategoriesRepository } from "../../modules/cars/repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../../modules/cars/repositories/implementations/ICategoriesRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/implementations/ISpecificationsRepository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/SpecificationRepository";
+
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository",
+  CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
+);
