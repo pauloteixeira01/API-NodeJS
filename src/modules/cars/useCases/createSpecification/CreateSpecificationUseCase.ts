46ce4f6f5e0051892b44d964fc974e2e7f0f14f4
Paulo Teixeira <paulo.teixeira@fortbrasil.com.br>
2022-12-23 17:14:03 -0300

    feat: finish unit test

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { AppError } from "../../../../errors/AppError";
-import { ISpecificationsRepository } from "../../repositories/implementations/ISpecificationsRepository";
+import { AppError } from "@shared/errors/AppError";
+import { ISpecificationsRepository } from "@modules/cars/repositories/implementations/ISpecificationsRepository";
 
 interface IRequest {
   name: string;
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,11 @@
 import { container } from "tsyringe";
 
-import { IUsersRepository } from "../../modules/accounts/repositories/implementations/IUsersRepository";
-import { UsersRepository } from "../../modules/accounts/repositories/UsersRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/CategoriesRepository";
-import { ICategoriesRepository } from "../../modules/cars/repositories/implementations/ICategoriesRepository";
-import { ISpecificationsRepository } from "../../modules/cars/repositories/implementations/ISpecificationsRepository";
-import { SpecificationsRepository } from "../../modules/cars/repositories/SpecificationRepository";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/implementations/IUsersRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/implementations/ICategoriesRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/implementations/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
